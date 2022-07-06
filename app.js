199afdfc947add4db458b712c69935c11021328a
temuge <e.temuge@yahoo.com>
2022-07-06 21:54:38 +0800

    finance module ruu item ad hiideg bolgono

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var uiController = (function(){
     return {
         getInput: function() {
             return {
-                type: document.querySelector(DOMstrings.inputType).value,
+                type: document.querySelector(DOMstrings.inputType).value, // exp , inc
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
@@ -36,9 +36,9 @@ var financeController = (function(){
         this.description = description;
         this.value = value;
     };
-
+    // private data
     var data = {
-        allItems: {
+        items: {
             inc: [],
             exp: []
         },
@@ -46,6 +46,31 @@ var financeController = (function(){
         totals: {
             inc: 0,
             exp: 0
+        }
+    }
+
+    return {
+        addItem: function(type, desc, val) {
+
+            var item, id;
+
+            // [1, 3]
+
+            if(data.items[type].length === 0) id = 1;
+            else {
+                id = data.items[type][data.items[type].length -1].id + 1;
+            }
+
+            if(type === 'inc') {
+                item = new Income(id, desc, val);
+            } else {
+                item = new Expense(id, desc, val);
+            }
+            
+            data.items[type].push(item);
+        },
+        data: function() {
+            return data;
         }
     }
 })();
@@ -57,9 +82,9 @@ var appController = (function(uiController, financeController){
     
     var ctrlAddItem = function() {
         // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-        console.log(uiController.getInput())
+        var input = uiController.getInput()
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-
+        financeController.addItem(input.type, input.description, input.value)
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
 
         // 4. Төсвийг тооцоолно
